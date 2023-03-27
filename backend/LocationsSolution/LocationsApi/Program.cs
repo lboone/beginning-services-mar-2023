e3b62517c747de627567a640272dd189fbb0c05c
Lloyd Boone <lboone@progressive.com>
2023-03-27 15:12:01 -0400

    services and models added

--- backend/LocationsSolution/LocationsApi/Program.cs
+++ backend/LocationsSolution/LocationsApi/Program.cs
@@ -1,3 +1,5 @@
+using LocationsApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var clock = new UptimeClock();
+builder.Services.AddSingleton<UptimeClock>(clock);
 
 var app = builder.Build();
 
