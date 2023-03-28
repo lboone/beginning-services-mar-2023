49728958616e95c8c71016a3d377358fd51b843e
Lloyd Boone <lboone@progressive.com>
2023-03-28 16:39:41 -0400

    Docker yml

--- backend/LocationsSolution/LocationsApi/Program.cs
+++ backend/LocationsSolution/LocationsApi/Program.cs
@@ -1,5 +1,7 @@
+using LocationsApi;
 using LocationsApi.Adapters;
 using LocationsApi.Services;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,16 +15,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var onCallAddress = builder.Configuration.GetValue<string>("onCallAddress");
-
-if(onCallAddress is null)
+if (onCallAddress is null)
 {
     throw new Exception("Can't start API without the onCallAddress");
 }
-
+Console.WriteLine($"Using the API address of {onCallAddress}");
 builder.Services.AddHttpClient<OnCallDeveloperHttpAdapter>(client =>
 {
     client.BaseAddress = new Uri(onCallAddress); // TODO DON'T DO THIS.
-});
+}).AddPolicyHandler(SrePolicies.GetDefaultRetyPolicyAsync())
+.AddPolicyHandler(SrePolicies.GetDefaultCircuitBreaker());
 
 var clock = new UptimeClock();
 builder.Services.AddSingleton<UptimeClock>(clock);
@@ -35,6 +37,18 @@ builder.Services.AddCors(options =>
         pol.AllowAnyHeader();
         pol.AllowAnyMethod();
     });
+});
+
+var locationsConnectionsString = builder.Configuration.GetConnectionString("locations-db");
+if(locationsConnectionsString is null)
+{
+    throw new Exception("Can't start up without a connection string to the database!");
+
+}
+builder.Services.AddMarten(options =>
+{
+    options.Connection(locationsConnectionsString);
+    options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
 });
 
 var app = builder.Build();
