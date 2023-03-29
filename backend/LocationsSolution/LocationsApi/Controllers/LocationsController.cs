7cfebe465c0e215c1f35c47f000c597a46c13510
Lloyd Boone <lboone@progressive.com>
2023-03-29 12:39:54 -0400

    docker

--- backend/LocationsSolution/LocationsApi/Controllers/LocationsController.cs
+++ backend/LocationsSolution/LocationsApi/Controllers/LocationsController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LocationsApi.Models;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LocationsApi.Controllers;
 
 public class LocationsController : ControllerBase
 {
+    private readonly IDocumentSession _context;
+    public LocationsController(IDocumentSession context)
+    {
+        _context = context;
+    }
     [HttpGet("/locations")]
     public async Task<ActionResult>  GetLocations()
     {
-        return Ok();
+        var data = await _context.Query<LocationItemResponse>().ToListAsync();
+        var response = new LocationsResponse { _embedded = data };
+        return Ok(response);
+    }
+
+    [HttpPost("/locations")]
+    public async Task<ActionResult> AddLocation([FromBody] LocationCreate request)
+    {
+        if(!ModelState.IsValid) {
+            return BadRequest(ModelState);
+        }
+
+        var locationToAdd = new LocationItemResponse
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Id = Guid.NewGuid().ToString(),
+            AddedBy = "Lloyd",
+            AddedOn = DateTime.Now,
+        };
+        _context.Store<LocationItemResponse>(locationToAdd);
+        // add all the things.
+
+        await _context.SaveChangesAsync();
+        return Ok(locationToAdd);
     }
 }
--- backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
+++ backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
@@ -1,17 +1,27 @@
-﻿namespace LocationsApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocationsApi.Models;
 
 
 public record LocationsResponse
 {
-    public List<LocationsResponse> _embedded { get; set; } = new();
-    public record LocationItemResponse
-    {
-        public string Id { get; init; } = string.Empty;
-        public string Name { get; init; } = string.Empty;
-        public string Description { get; init; } = string.Empty;
-        public string AddedBy { get; init; } = string.Empty;
-        public DateTime AddedOn { get; init; }
-    }
+    public IReadOnlyList<LocationItemResponse>? _embedded { get; set; }
+}
+public record LocationItemResponse
+{
+    public string Id { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string AddedBy { get; init; } = string.Empty;
+    public DateTime AddedOn { get; init; }
+}
+
+public record LocationCreate
+{
+    [Required, MaxLength(75)]
+    public string Name { get; set; } = string.Empty;
+    [Required, MaxLength(1000)]
+    public string Description { get; set; } = string.Empty;
 }
 /*  {
     id: '1',
